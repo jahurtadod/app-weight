7b58d5d904bc7ba20ac07266ce66037dfd22463b
Paul Ramon <peramon98.18@gmail.com>
2025-09-30 10:17:02 -0500

    feat: Check for null values in the user model

--- lib/user/data/models/user_model.dart
+++ lib/user/data/models/user_model.dart
@@ -10,7 +10,6 @@ UserModel welcomeFromJson(String str) => UserModel.fromJson(json.decode(str));
 String welcomeToJson(UserModel data) => json.encode(data.toJson());
 
 class UserModel extends User {
-
   UserModel({
     required super.id,
     required super.name,
@@ -22,13 +21,21 @@ class UserModel extends User {
 
   factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
     // TODO: En caso de ser nulo que va hacer ?
-    id: json["id"],
+    id: (json["id"] ?? "") as String,
     name: json["name"],
-    lastName: json["lastName"],
-    email: json["email"],
-    phoneNumber: json["phoneNumber"],
-    role: ROL.values.byName(json["role"]),
+    lastName: (json["lastName"] ?? "") as String,
+    email: (json["email"] ?? "") as String,
+    phoneNumber: (json["phoneNumber"] ?? "") as String,
+    role: _roleFromJson(json["role"]),
   );
+
+  static ROL _roleFromJson(dynamic raw) {
+    if (raw is String) {
+      final idx = ROL.values.indexWhere((e) => e.name == raw);
+      if (idx != -1) return ROL.values[idx];
+    }
+    return ROL.user; // default
+  }
 
   Map<String, dynamic> toJson() => {
     "id": id,
